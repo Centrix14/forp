5af3de1601e1cdc0f89523383a1dbb40bdc2b4b2
Centrix14 <artiom.zvyagin@yandex.ru>
2021-04-19 19:01:33 +0300

    added new features

--- lib.h
+++ lib.h
@@ -5,8 +5,20 @@ void ll_exec(list *node);
 void ll_run_std(int code, list *node);
 
 int ll_is_std(char *name);
+void ll_make_ret(list *node);
 
 void ll_cb_print(list *node);
 void ll_cb_help(list *node);
+void ll_cb_nil(list *node);
+void ll_cb_sum(list *node);
+void ll_cb_sub(list *node);
+void ll_cb_mul(list *node);
+void ll_cb_div(list *node);
+
+// for internal using only!
+void __sum(double *acc, double arg);
+void __sub(double *acc, double arg);
+void __mul(double *acc, double arg);
+void __div(double *acc, double arg);
 
 #endif
