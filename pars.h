1756545183432c968884d2da7b7a02e621b94e9c
Centrix14 <artiom.zvyagin@yandex.ru>
2021-04-19 19:01:48 +0300

    interactive mode

--- pars.h
+++ pars.h
@@ -2,7 +2,11 @@
 #define __FORK_PROCESSOR_LANGUAGE_PARSER_LIBRARY_FILE_H_INCLUDED__
 
 int pl_get_index(char *str);
-void pl_pars_file(list *tree, char *file_name);
+
+void pl_pars_stream(list *tree, FILE *file);
 void pl_pars_line(list *tree, char *line);
+
+void pl_print_welcome();
+void pl_prompt(FILE *stream);
 
 #endif
