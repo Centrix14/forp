1cc38e0db41ad70a68473d0a67c2c3d1c1f7e541
Centrix14 <artiom.zvyagin@yandex.ru>
2021-04-21 19:02:50 +0300

    some changes

--- var.h
+++ var.h
@@ -13,12 +13,14 @@ typedef struct {
 	int tag;
 } var;
 
+void vl_scope_list_init();
 void vl_scope_free();
 int vl_scope_new(char *name);
 int vl_scope_check(char *name);
 int vl_scope_get_tag(char *name);
 
-void vl_var_add(char *name, char *value, char *scope_name);
+void vl_var_list_init();
+void vl_var_add(char *name, char *value, int tag);
 var *vl_var_get(char *name, char *scope_name);
 char *vl_var_get_value(char *name, char *scope_name);
 void vl_var_list_free();
