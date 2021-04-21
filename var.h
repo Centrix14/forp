dc24f0898ef61853ca0a822008a0b5eaa4371bdc
Centrix14 <artiom.zvyagin@yandex.ru>
2021-04-21 18:26:55 +0300

    variable library

---
+++ var.h
@@ -0,0 +1,26 @@
+#ifndef __FORK_PROCESSOR_LANGUAGE_VARIABLE_LIBRARY_HEADER_FILE_INCLUDED__
+#define __FORK_PROCESSOR_LANGUAGE_VARIABLE_LIBRARY_HEADER_FILE_INCLUDED__
+
+typedef struct {
+	char *name;
+	int tag;
+} scope;
+
+typedef struct {
+	char *name;
+	char *value;
+
+	int tag;
+} var;
+
+void vl_scope_free();
+int vl_scope_new(char *name);
+int vl_scope_check(char *name);
+int vl_scope_get_tag(char *name);
+
+void vl_var_add(char *name, char *value, char *scope_name);
+var *vl_var_get(char *name, char *scope_name);
+char *vl_var_get_value(char *name, char *scope_name);
+void vl_var_list_free();
+
+#endif
