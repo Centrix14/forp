7063da5f37fe15e671b18821a5fdffa671e8442c
Centrix14 <artiom.zvyagin@yandex.ru>
2021-04-23 19:00:37 +0300

    some new functions, and increase safety

--- var.c
+++ var.c
@@ -35,7 +35,7 @@ int vl_scope_check(char *name) {
 	int tag = 0;
 
 	tag = vl_scope_get_tag(name);
-	if (tag > 0)
+	if (tag >= 0)
 		return tag;
 	else
 		return vl_scope_new(name);
@@ -99,6 +99,23 @@ void vl_scope_free() {
 
 void vl_scope_list_init() {
 	scope_list = list_init_node(NULL);
+}
+
+char *vl_scope_get_name(int tag) {
+	list *node = NULL;
+	scope *sc = NULL;
+
+	node = scope_list;
+	while (node) {
+		sc = (scope*)node->data;
+		if (sc)
+			if (sc->tag == tag)
+				return sc->name;
+
+		node = node->next;
+	}
+
+	return NULL;
 }
 
 void vl_var_list_init() {
@@ -194,4 +211,62 @@ void vl_var_list_free() {
 
 		node = next;
 	}
+}
+
+void vl_var_list_show() {
+	list *node = NULL;
+	var *vptr = NULL;
+
+	node = var_list;
+	while (node) {
+		if (node->data) {
+			vptr = (var*)node->data;
+			if (vptr)
+				printf("[%d] %s = %s\n", vptr->tag, vptr->name, vptr->value);
+		}
+
+		node = node->next;
+	}
+}
+
+int vl_var_get_exist(char *name, char *scope_name) {
+	if (vl_scope_get_tag(scope_name) == -1)
+		return 0;
+	if (!vl_var_get(name, scope_name))
+		return 0;
+	return 1;
+}
+
+void vl_var_change_value(char *name, char *value, char *scope_name) {
+	var *vptr = NULL;
+
+	vptr = vl_var_get(name, scope_name);
+	if (vptr) {
+		vptr->value = (char*)realloc(vptr->value, strlen(value) + 1);
+		if (!vptr->value) {
+			perror("forp");
+
+			exit(0);
+		}
+
+		strcpy(vptr->value, value);
+	}
+}
+
+var *vl_var_get_exist_with_syntax(char *name) {
+	char *scope_name, *variable_name;
+
+	if (strchr(name, ':')) {
+		scope_name = strtok(name, ":");
+		variable_name = strtok(NULL, "\n\b\r\t ");
+	}
+	else {
+		scope_name = "global";
+		variable_name = name;
+	}
+
+	if (!variable_name || !scope_name)
+		return NULL;
+
+	return vl_var_get(variable_name, scope_name);
 }
