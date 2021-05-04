760a7e3f290a122c85f534df8e3868bbb1b1dd7e
Centrix14 <artiom.zvyagin@yandex.ru>
2021-05-04 15:28:10 +0300

    new functions

--- scope.h
+++ scope.h
@@ -3,7 +3,9 @@
 
 typedef struct {
 	char *name;
+
 	int tag;
+	int call_level;
 } scope;
 
 int sl_scope_check(list *scope_list, char *name);
@@ -13,9 +15,13 @@ void sl_scope_free(list *scope_list);
 
 char *sl_scope_get_name(list *scope_list, int tag);
 int sl_scope_get_tag(list *scope_list, char *name);
+char *sl_scope_get_by_call_level(list *node, int level);
 
 int sl_scope_new(list *scope_list, char *name);
 void sl_scope_remove(list *scope_list, int tag);
 void sl_scope_delete(list *node, scope *sc);
+
+void sl_scope_call(list *node, char *name);
+void sl_scope_revoke(list *node, char *name);
 
 #endif
