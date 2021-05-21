77fb656ce26a04c3447f39139a60bb94d3554f6c
Centrix14 <artiom.zvyagin@yandex.ru>
2021-05-21 15:12:41 +0300

    work with recursion

--- var.h
+++ var.h
@@ -11,15 +11,16 @@ typedef struct {
 void vl_var_add(char *name, char *value, int tag);
 void vl_var_remove(char *name, int tag);
 
-void vl_var_change_value(char *name, char *value, char *scope_name);
+void vl_var_change_value(char *name, char *value, int tag);
 
 void vl_var_list_free();
 void vl_var_list_show();
 void vl_var_list_init();
 
-int vl_var_get_exist(char *name, char *scope_name);
-var *vl_var_get(char *name, char *scope_name);
-char *vl_var_get_value(char *name, char *scope_name);
+var *vl_var_get(char *name, int tag);
+char *vl_var_get_value(char *name, int tag);
+
+int vl_var_get_exist(char *name, int tag);
 var *vl_var_get_exist_with_syntax(char *name);
 
 void vl_var_free(list *node, var *vptr);
